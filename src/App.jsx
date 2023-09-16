c843e2f682a534223a8253de2e9de8d19983197d
tuan782000 <thaituan7820@gmail.com>
2023-09-16 07:59:10 +0700

    logic code in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Employee from "./components/Employee"
 function App() {
-  
-
+  // Thử code logic js trong này
+  console.log("We are about to list the employee");
+  // Giải thích: cho biến lưu giá trị true or false, 
+  const checkEmployee = true;
   return (
+    // dùng toán tử 3 ngôi check nếu true thì in ra nguyên cụm fragments <></> còn false in ra <p>
     <>
-      <h1>Hello World</h1>
-      <Employee />
+      {checkEmployee ? 
+        <>
+          <h1>Hello World</h1>
+          <Employee />
+          <Employee />
+          <Employee />
+        </> 
+        : 
+        <p>You can not not see an employee</p>}
     </>
   )
 }
