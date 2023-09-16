ba5c686b895aed930d3ee1e3babd825315dd7c56
tuan782000 <thaituan7820@gmail.com>
2023-09-16 09:31:05 +0700

    create tag with html and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,27 @@ function App() {
   const [role, setRole] = useState("Manager"); // giá trị ban đầu set cho nó là Manager
   return (
     <>
-      {checkEmployee ? 
+      {checkEmployee ?
         <>
-          <h1 className="bg-red-300">Hello World</h1>
           {/* Cái e là nội dung người dùng gõ - đưa vào làm đối cho hàm - onChange này Call back function */}
           <input type="text" onChange={(e) => {
             console.log(e.target.value)
             // Nhập giá trị vào ô input thì thực hiện callback đồng thời thực chức năng useState, setRole, cập nhật lại role ban đầu
-            setRole(e.target.value) 
+            setRole(e.target.value)
           }} />
-          <Employee name="Tuan" role={role}/>
-          <Employee name="Nguyen"  role="Intern"/>
-          <Employee name="Thai" />
-        </> 
-        : 
+          <div className="flex flex-wrap justify-center">
+            <Employee name="Anh" role={role} img="https://images.pexels.com/photos/1391499/pexels-photo-1391499.jpeg" />
+            <Employee name="Binh" role="Intern" img="https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Thuy" img="https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+            <Employee name="Tuyet" role={role} img="https://images.pexels.com/photos/1391495/pexels-photo-1391495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+            <Employee name="Nguyen" role="Intern" img="https://images.pexels.com/photos/1372137/pexels-photo-1372137.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Thai" img="https://images.pexels.com/photos/1382730/pexels-photo-1382730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Tuan" role={role} img="https://images.pexels.com/photos/1391494/pexels-photo-1391494.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Nguyen" role="Intern" img="https://images.pexels.com/photos/1382730/pexels-photo-1382730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Thai" img="https://images.pexels.com/photos/3616937/pexels-photo-3616937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </div>
+        </>
+        :
         <p>You can not not see an employee</p>}
     </>
   )
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/prop-types */
 const Employee = (props) => {
   return (
-    // Một chút lưu ý: Để chạy code JS bên trong cần có cặp ngoặc nhọn bao bọc {code js in here }
-    // Nếu truyền có thì hứng, không có thì thui không hiển thị
-    <>
-      <div>Name employee is {props.name}</div>
-      {/* có thể viết như này */}
-      {/* <p>{props.role ? props.name : "No Role"}</p> */}
-      {/* Hoặc */}
-      {props.role ? <p className="role">{props.role}</p> : <p className="noRole">No role</p>}
-    </>
+    <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img className="object-cover rounded-full h-[100px] w-[100px] block mx-auto sm:mx-0 sm:shrink-0" src={props.img} alt="Image Avater" />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">
+            {props.name}
+          </p>
+          <p className="text-slate-500 font-medium">
+            {props.role}
+          </p>
+        </div>
+        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+      </div>
+    </div>
 
   )
 }
